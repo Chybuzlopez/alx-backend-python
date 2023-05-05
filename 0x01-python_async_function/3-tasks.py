674ed4af725606e2627d32cf012b7638bd467d00
Chybuzlopez <danielorihanna@gmail.com>
2023-05-05 14:32:23 +0000

    task done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 """
-A function (do not create an async function, use the regular function syntax to do this)
-task_wait_random that takes an integer max_delay and returns an asyncio.Task.
+A function (do not create an async
+function, use the regular function
+syntax to do this)task_wait_random
+that takes an integer max_delay
+and returns an asyncio.Task.
 """
 
 import asyncio
